4fbb3c21960921f89a1679b087508f4ce41ef153
Claire2003 <96359979+Claire2003@users.noreply.github.com>
2023-09-25 23:33:43 +1000

    Update infinite_hash_table.py
    did setitem method

--- infinite_hash_table.py
+++ infinite_hash_table.py
@@ -20,8 +20,9 @@ class InfiniteHashTable(Generic[K, V]):
 
     TABLE_SIZE = 27
 
-    def __init__(self) -> None:
-        raise NotImplementedError()
+    def __init__(self,level) -> None:
+        self.level = level
+        self.array:ArrayR[tuple[K, V]] = ArrayR(self.TABLE_SIZE) 
 
     def hash(self, key: K) -> int:
         if self.level < len(key):
@@ -34,13 +35,49 @@ class InfiniteHashTable(Generic[K, V]):
 
         :raises KeyError: when the key doesn't exist.
         """
+        key1, key2 = key
+
+        if self.__contains__(key):
+            table_position, sub_table_position = self._linear_probe(key1, key2, False)
+            return self.table[table_position][1].array[1]
+        raise KeyError(key1)
+
         raise NotImplementedError()
 
     def __setitem__(self, key: K, value: V) -> None:
         """
         Set an (key, value) pair in our hash table.
         """
-        raise NotImplementedError()
+        index = hash(key)
+        
+        # check the index
+        if self.array[index] != None: 
+            if type(self.array[index][1]) != list: # checking if the index has a value or an array
+                store = self.array[index] # storing the value
+                self.array[index] = None # erasing the value
+                new_key = key[self.level]
+                nextHashTable = InfiniteHashTable(self.level+1)
+                self.array[index] = (new_key, nextHashTable) # adding the 
+                nextHashTable.__setitem__(key, value)
+                nextHashTable.__setitem__(store[0], store[1])
+                """
+                erase value
+                make new value first letter of key
+                make instance of the hashtable class
+                nexthashtable = InfiniteHashTable(self.level+1)
+                add key
+                add erased value
+                """
+                
+            else:
+                nextHashTable = self.array[index][1]
+                nextHashTable.__setitem__(key, value)
+                """
+                go into next hash table
+                hash second letter of the key
+                """
+        else:
+            self.array[index] = (key,value)
 
     def __delitem__(self, key: K) -> None:
         """
