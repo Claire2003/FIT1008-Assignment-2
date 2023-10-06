5e6c2d425117e49dc36a8d7bf8c8b2e64ff4c08d
Daruler24 <Daruler24@users.noreply.github.com>
2023-10-06 14:21:01 +1100

    Final product
    Passes all test cases and all done

--- mountain_organiser.py
+++ mountain_organiser.py
@@ -1,117 +1,121 @@
+"""
+Mountain Organiser class. A class that sorts mountains and stores their information.
+"""
 from __future__ import annotations
+__author__ = "Haru Le"
 
 from mountain import Mountain
-
-from double_key_table import DoubleKeyTable
 
 from infinite_hash_table import InfiniteHashTable
 
 from data_structures.hash_table import LinearProbeTable
-from algorithms.binary_search import binary_search
+
 from algorithms.mergesort import mergesort, merge
 
 class MountainOrganiser:
     """
     Mountain Organiser.
-    Implementation Facts:
-    - Infinite Hash Table should be used to sort lexicographically
+
+    Unless stated otherwise, all methods have O(1) complexity.
     """
     def __init__(self) -> None:
-        # Dictionary
-        self.dictionary = LinearProbeTable()
-        # Actual organised list
-        self.organiser = list()
+        """ Initialise Mountain Organiser. """
+        # Hash table of mtns and rank. Key: mtn name Value: Rank
+        self.hash_table = LinearProbeTable()
+        # Actual organised list of mountains
+        self.organiser = []
 
     def cur_position(self, mountain: Mountain) -> int:
         """
         Finds the rank of the provided mountain given all mountains included so far. 
+
+        :mountain: Search mountain. 
+
         :raises KeyError: If this mountain hasn't been added yet.
-        :comp: O(log(N)), where N is the total number of mountains so far.
+
+        :complexity: O(1)
         """
-        # if self.dictionary.__contains__[mountain.name]:
-            # log(N) comes from binary search
-        return self.dictionary[mountain.name]
+        return self.hash_table[mountain.name]
 
     def add_mountains(self, mountains: list[Mountain]) -> None:
         """
-        :comp: O(Mlog(M) + N), M length of input list, N total number of mountains so far.
+        Adds a list of mountains to the organiser.
+
+        :mountains: List of mountains to be added to organiser.
+
+        :complexity: O(Mlog(M) + N)
+        Where M is the length of input list and N is the total number of mountains so far.
         """
-        # Hash table is to count how many mtns in diff level. 
+        # Counts hash table is to count how many mtns in each diff level. 
         counts_hash_table = LinearProbeTable()
         counts_hash_table.hash = lambda k: k % counts_hash_table.table_size
-        # Mlog(M) complexity comes from using merge sort to sort the input list
-        # Then it would be M to add it to the original list, but that cancels out.
-        # N complexity comes from merging the whole list back together again.
-
         # O(Mlog(M))
+        # Sort before adding. 
         sorted_mountains = mergesort(mountains, lambda x:x.difficulty_level)
-        # O(N)
+        # O(M+N) (This cancels out in the grand scheme of things)
+        # Because input is already sorted, we can use mergesorts merge method which is M+N comp to combine the two sorted lists.
         self.organiser = merge(self.organiser, sorted_mountains, lambda x:x.difficulty_level)
-        for mtn in self.organiser:
-            print(mtn.difficulty_level, mtn.name)
         # So now everything is sorted by difficulty level
         # We should go through the whole list again and sort lexicographically
-        # O(N)
+        # O(M+N)
         for mtn in self.organiser:
             key = mtn.difficulty_level
             try:
+                # Key exists
                 counts_hash_table[key] += 1
             except:
+                # Key doesnt exist
                 counts_hash_table[key] = 1
         
-        # This should also be O(N)
-        # Use an iterator? Might lessen complexity
-        print(counts_hash_table)
+        # O(M+N), goes over all elements in organiser. 
+        # Only if there are multiple mtns under the same diff level, 
+        # Do we sort lexicographically. 
+        # Doesn't become n^2 complexity as the greater there are under 1 diff level
+        # The less there will be under another diff level.
         for diff_level in counts_hash_table.keys():
             names = InfiniteHashTable()
             if counts_hash_table[diff_level] > 1:
                 for rank, mtn in enumerate(self.organiser):
                     if mtn.difficulty_level == diff_level:
-                        print(rank, mtn.name)
                         names[mtn.name] = (mtn, rank)
             if len(names) > 0:
+                # Sort names
                 sorted_names = names.sort_keys()
-                for x, name in enumerate(sorted_names):
+                # Use their sorted index to rank them in the organiser.
+                for sorted_index, name in enumerate(sorted_names):
                     mtn_info = names[name]
                     mtn_object, index = mtn_info
-                    if x == 0:
+                    if sorted_index == 0:
                         lowest_rank = index
                     else:
                         lowest_rank += 1
                     self.organiser[lowest_rank] = mtn_object
-        
+        # Add to hash table for cur position method. 
+        # O(M+N)
         for rank, mtn in enumerate(self.organiser):
-            self.dictionary[mtn.name] = rank
-
-    def binary_search(self, mtn_list, mtn) -> int:
+            self.hash_table[mtn.name] = rank
+        
+    def remove_mountain(self, mountain: Mountain) -> None:
         """
-        Utilise the binary search algorithm to find the index where a particular element would be stored.
+        Remove a mountain from the organiser.
 
-        :return: The index at which either:
-            * This item is located, or
-            * Where this item would be inserted to preserve the ordering.
-
-        :complexity:
-        Best Case Complexity: O(1), when middle index contains item.
-        Worst Case Complexity: O(log(N)), where N is the length of l.
+        :mountain: Mountain to be removed.
+        
+        :complexity best: O(1)
+        :complexity worst: O(n)
+        Where n is the number of mountains in the organiser.
+        Best case: Remove the highest rank mountain. No need to shuffle. Or change higher ranks in hash table.
+        Worst case: Remove the lowest rank mountain. Shuffle all elements to the left one, change all remaining mtns rank in hash table.
         """
-        return self._binary_search_aux(mtn_list, mtn, 0, len(mtn_list))
-
-    def _binary_search_aux(self, mtn_list, mtn, lo: int, hi: int) -> int:
-        """
-        Auxilliary method used by binary search.
-        lo: smallest index where the return value could be.
-        hi: largest index where the return value could be.
-        """
-        if lo == hi:
-            return lo
-        mid = (hi + lo) // 2
-        if mtn_list[mid].difficulty_level > mtn.difficulty_level:
-            # Item would be before mid
-            return self._binary_search_aux(mtn_list, mtn, lo, mid)
-        elif mtn_list[mid].difficulty_level < mtn.difficulty_level:
-            # Item would be after mid
-            return self._binary_search_aux(mtn_list, mtn, mid+1, hi)
-        elif mtn_list[mid].difficulty_level == mtn.difficulty_level:
-            return mid
-        raise ValueError(f"Comparison operator poorly implemented {mtn} and {mtn_list[mid]} cannot be compared.")
+        # Get sublist from ahead of it.
+        remove_position = self.cur_position(mountain)
+        sub_list = self.organiser[remove_position+1:] # O(length of slice) < O(n)
+        # Remove from list
+        # Complexity of remove in python list is O(n), as it shuffles all elements on the right to the left one. 
+        self.organiser.remove(mountain) 
+        # Remove from dict O(1)
+        self.hash_table.__delitem__(mountain.name)
+        # O(n)
+        for mtn in sub_list:
+            # Decrement rank.
+            self.hash_table[mtn.name] -= 1
--- trail.py
+++ trail.py
@@ -1,5 +1,5 @@
 """
-Trail class. Stores information regarding type of trail, and trail operations.
+Trail class. Stores information regarding type of trail, and provides trail operations.
 """
 from __future__ import annotations
 __author__ = "Haru Le"
@@ -12,6 +12,7 @@ from typing import TYPE_CHECKING, Union
 # Avoid circular imports for typing.
 if TYPE_CHECKING:
     from personality import WalkerPersonality
+    from data_structures.linked_stack import LinkedStack
 
 @dataclass
 class TrailSplit:
@@ -110,7 +111,9 @@ class Trail:
 
         :personality: Personality that indicates the branches to choose. WalkerPersonality.
 
-        :complexity: Best O(1) Worst O(n), where n is the number of trails inside of the path.
+        :complexity best: O(1) 
+        :complexity worst: O(n)
+        Where n is the length of the longest path of trails inside of the whole track.
         Best case: Lazy Personality encounters top and bottom series of equal difficulty at the start of a trail.
         Worst case: Walker goes through all trails.
         """
@@ -175,11 +178,10 @@ class Trail:
 
     def collect_all_mountains(self) -> list[Mountain]:
         """Returns a list of all mountains on the trail.
-        Does this by using recursion and getting all mountains from the next sequential trail.
-        :current_trail: The current trail we want to extract following mountains from. 
-        :following_splits: Linked Stack that holds all following trails from splits. 
-        :comp: O(n) 
-        Where n is the number of mountains and branches combined in the path. 
+
+        :return: all_mountains, list of mountains on the trail. 
+        
+        :complexity: O(_collect_all_mountains_aux())
         """
         from data_structures.linked_stack import LinkedStack
         all_mountains = []
@@ -187,88 +189,114 @@ class Trail:
         self._collect_all_mountains_aux(self.store, following_splits, all_mountains)
         return all_mountains
     
-    from data_structures.linked_stack import LinkedStack
     def _collect_all_mountains_aux(self, current_trail: TrailStore, following_splits: LinkedStack, all_mountains: list[Mountain]):
+        """
+        Adds to a list of all mountains following the current_trail.
+        Done using recursion and getting all mountains from the next sequential trail.
+        :current_trail: The current trail we want to extract following mountains from. 
+        :following_splits: Linked Stack that holds all following trails from splits. 
+        :all_mountains: Accumulator that holds mountains we want to store.
+
+        :complexity best: O(1) (current_trail is final trail)
+        :complexity worst: O(n)
+        Where n is the highest number of trails that come after current_trail.
+        """
         from data_structures.linked_stack import LinkedStack
         # Base case?
         # When there are no more following splits to go through and we reach the end of a series/nothing more to look at.
         if following_splits.is_empty() and current_trail is None:
             return
-
+        
         if type(current_trail) is TrailSeries:
+            # Add to accum if current_trail is a series. As it must have a mountain. 
             all_mountains.append(current_trail.mountain)
+            # Since it is a series, we analyse the next trail in the series.
             self._collect_all_mountains_aux(current_trail.following.store, following_splits, all_mountains)
         elif type(current_trail) is TrailSplit:
+            # When a split occurs, we want to store following for after we have gone through the split
+            # The way it is implemented here, we will go through the top branch until we have reached the end.
+            # After that, we go through the bottom branch just to make sure we captured everything, 
+            # However we don't go to the end, as that would result in repeating mountains
             following = current_trail.following.store
             top = current_trail.top.store
             bottom = current_trail.bottom.store
             following_splits.push(following)
-            # Create two following splits.
-            # temp_stack = LinkedStack() 
-            # dupe_splits_1 = LinkedStack()
-            # dupe_splits_2 = LinkedStack()
-            # while not following_splits.is_empty():
-            #     temp_stack.push(following_splits.pop())
-
-            # while not temp_stack.is_empty():
-            #     trail = temp_stack.pop()
-            #     print(type(trail))
-            #     dupe_splits_1.push(trail)
-            #     dupe_splits_2.push(trail)
-
-            # The reason why we don't maintain the splits, is because, once we have gone all the way to the end,
-            # We don't want to repeatedly come out of the splits.
-            # This would end up in duplicate elements. 
-            # Luckily we can just leave as is. 
             self._collect_all_mountains_aux(top, following_splits, all_mountains)
             self._collect_all_mountains_aux(bottom, following_splits, all_mountains)
         elif current_trail is None:
+            # Because following splits is not empty and current trail is not None, 
+            # It means that we must have reached the end of a split, but not the whole trail.
+            # Therefore, we want to continue, and keep collecting mountains from the following of the prior split we went into. 
             if not following_splits.is_empty():
                 self._collect_all_mountains_aux(following_splits.pop(), following_splits, all_mountains)
 
     def difficulty_maximum_paths(self, max_difficulty: int) -> list[list[Mountain]]: # Input to this should not exceed k > 50, at most 5 branches.
-        # 1008/2085 ONLY!
-        # Try figure out why input size is so important, I suspect that it's because it might make us choose which data structure to use.
-        # If we know the max size, then Stack is better.
+        """
+        Given an integer max_difficulty, calculate all paths through the trail such that the maximum difficulty of all mountains in the path does not exceed maximum_difficulty.
+        The return value of this should be a list containing lists, containing the Mountains on each path, in order taken in the path.
 
-        # Need recursive function that returns a list of possible paths (list of trails) that we could take.
-        # Either while we recurse, we filter out the ones that don't fit the requirement, 
-        # Or at the end, we have difficulty level's associated with each path.
-        
+        Paths are considered distinct if any branch decision chosen is different, even if the mountains traversed would be the same.
+
+        :return: paths list[list[Mountain]] object of all paths that satisfy the requirement.
+
+        :complexity: O(_difficulty_maximum_paths_aux())
+        """
         from data_structures.linked_stack import LinkedStack
         paths = []
         following_splits = LinkedStack()
+        # Start from the start, and ensure current path is nothing before accumulating
         self._difficulty_maximum_paths_aux(max_difficulty, self.store, following_splits, [] , paths)
         return paths
 
     def _difficulty_maximum_paths_aux(self, max_difficulty: int, current_trail: TrailStore, following_splits: LinkedStack, current_path: list[Mountain], paths: list[list[Mountain]]):
-        from data_structures.linked_stack import LinkedStack
-        # Start with trying to make recursive algorithm to get all paths. 
-        # How to get a single path? 
-        # It's like collecting all mountains, but we want to regain the splits, and we want to add everything to a path list in the process.
+        """
+        Adds to a list of all paths that satisfy the max_difficulty condition.
+        Done using recursion and getting paths from the next sequential trail, and checking the condition.
+        :max_difficulty: Integer that represents the maximum difficulty any mountain in the paths can be. 
+        :current_trail: The current trail we want to extract following paths from. 
+        :following_splits: Linked Stack that holds all following trails from splits. 
+        :current_path: List of mountains that represents a path.
+        :paths: List of list of mountains that holds all paths that satisfy the condition.
 
+        :complexity best: O(1)
+        :complexity worst: O(n*m)
+        Where n is the total number of trails that come after current_trail.
+        Best case: current_trail is final trail
+        Worst case: current_trail is the start trail and you go through as many paths as there are branches. 
+        No path is invalid, so every path is considered. 
+        Visiting each trail multiple times. All the following trails get done twice. 
+        However if it not n^2, or other, rather just a multiple of the amount of trails in the whole track.
+        m comes from copying prior mountains when splitting paths.
+        Where m is the number of mountains in the current path. 
+        """
+        from data_structures.linked_stack import LinkedStack
+        # It's like collecting all mountains, but we want to regain the splits, and we want to add everything to a path list in the process.
         # Paths are distinct when different branches are chosen. 
         # Therefore, it can be thought that when encountering a branch, we create a new path object, with the same all prev elements in it. 
         # Then when we reach the end for both paths, we add them to paths.
         if following_splits.is_empty() and current_trail is None:
+            # Add to paths list, as we reached final. 
             paths += [current_path]
             return
 
         if type(current_trail) is TrailSeries:
             if current_trail.mountain.difficulty_level <= max_difficulty:
+                # Append mountains along the way.
                 current_path.append(current_trail.mountain)
                 self._difficulty_maximum_paths_aux(max_difficulty, current_trail.following.store, following_splits, current_path, paths)
             else:
+                # Reduces complexity, don't add to paths, and stop recursing from here. For this branch.
                 return
         elif type(current_trail) is TrailSplit:
             following = current_trail.following.store
             top = current_trail.top.store
             bottom = current_trail.bottom.store
             following_splits.push(following)
-            # Create two following splits.
+            # Create two following splits. To not lose the following split info when taking paths from top and bottom of split.
             temp_stack = LinkedStack() 
             dupe_splits_1 = LinkedStack()
             dupe_splits_2 = LinkedStack()
+            # Copying stacks can be thought of as constant as max branches are 5, and thus only 5 following branches possible.
             while not following_splits.is_empty():
                 temp_stack.push(following_splits.pop())
 
@@ -276,8 +304,10 @@ class Trail:
                 trail = temp_stack.pop()
                 dupe_splits_1.push(trail)
                 dupe_splits_2.push(trail)
-
-            current_path_copy = [trail for trail in current_path]
+            # Deep copy current path, as method relies on side effects.
+            # O(m) where m is the number of mountains in current_path
+            current_path_copy = [mtn for mtn in current_path]
+            # Create two new paths
             self._difficulty_maximum_paths_aux(max_difficulty, top, dupe_splits_1, current_path, paths)
             self._difficulty_maximum_paths_aux(max_difficulty, bottom, dupe_splits_2, current_path_copy, paths)
         elif current_trail is None:
